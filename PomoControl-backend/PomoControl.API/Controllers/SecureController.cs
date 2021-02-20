8139a2d44e991df1b133490c3706ae0b605b6733
HENRIQUE <henrique_holtz@hotmail.com>
2021-02-19 23:42:20 -0300

    Add endpoint 'api/secure?accesstoken=<Your AccesToken>'

---
+++ PomoControl-backend/PomoControl.API/Controllers/SecureController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace PomoControl.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : PomoController
+    {
+
+        [HttpGet]
+        //[Authorize]
+        [AllowAnonymous]
+        public object Secure([FromQuery]string accessToken)
+        {
+            //var stream = "[encoded jwt]";
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(accessToken);
+            var tokenS = handler.ReadToken(accessToken) as JwtSecurityToken; 
+
+
+            return new { tokenS };
+        }
+    }
+}
