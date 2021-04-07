259006d9d375518881e8f0eda05915116fd192a1
HENRIQUE <henrique_holtz@hotmail.com>
2021-04-07 19:49:03 -0300

    Revert "Adjustment: Using IConfiguration now. (before it was IConfigurationRoot)"
    This reverts commit 87874660613980bb003dc20131ddfa96ae5b2820.

--- PomoControl-backend/PomoControl.API/Startup.cs
+++ PomoControl-backend/PomoControl.API/Startup.cs
@@ -35,18 +35,18 @@ namespace PomoControl.API
 {
     public class Startup
     {
-        public Startup(/*Microsoft.AspNetCore.Hosting.IHostingEnvironment env */IConfiguration configuration)
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
-            Configuration = configuration;
-            //var builder = new ConfigurationBuilder()
-            //    .SetBasePath(env.ContentRootPath)
-            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            //    .AddEnvironmentVariables();
-            //Configuration = builder.Build();
+            //Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
-        public IConfiguration Configuration { get; }
-        //public IConfigurationRoot Configuration { get; }
+        //public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
