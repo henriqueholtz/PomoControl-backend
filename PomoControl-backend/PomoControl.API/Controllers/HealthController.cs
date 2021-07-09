5f4a0853329e55f7c04fc3e05da1b4540f4cd9bb
henriqueholtz <henrique_holtz@hotmail.com>
2021-07-08 23:37:54 -0300

    Add HealthController

---
+++ PomoControl-backend/PomoControl.API/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PomoControl.API.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Health()
+        {
+            await Task.Delay(150);
+            return Ok();
+        }
+    }
+}
